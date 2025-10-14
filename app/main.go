3254c19df6b755151e20930b94013212b9741e51
Milan Thakor <milanv.thakor@gmail.com>
2025-10-14 11:22:49 +0530

    Respond with 200

--- app/main.go
+++ app/main.go
@@ -17,9 +17,16 @@ func main() {
 		os.Exit(1)
 	}
 
-	_, err = l.Accept()
+	conn, err := l.Accept()
 	if err != nil {
 	 	fmt.Println("Error accepting connection: ", err.Error())
 	 	os.Exit(1)
+	}
+
+	// Write the response back to client
+	_, err = conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+	if err != nil {
+		fmt.Println("Error returning response: ", err.Error())
+		os.Exit(1)
 	}
 }
