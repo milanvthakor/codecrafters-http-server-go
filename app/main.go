e720b83de0c1ee25b066932ef5873f0a8b9503a4
Milan Thakor <milanv.thakor@gmail.com>
2025-10-19 16:05:44 +0530

    Return a file.

--- app/main.go
+++ app/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"io"
 	"net"
@@ -12,16 +13,29 @@ import (
 var (
 	CRLF             = "\r\n"
 	EchoEndpointRegx = regexp.MustCompile(`\/echo\/(?P<str>.*)`)
+	FileEndpointRegx = regexp.MustCompile(`\/files\/(?P<str>.*)`)
 )
+
+// RequestLine represents the details of the request like http method, target, and http version.
+type RequestLine struct {
+	HTTPMethod    string
+	RequestTarget string
+	HTTPVersion   string
+}
+
+// ConnHandler binds the connection with methods for parsing the request details and serving multiple endpoints
+type ConnHandler struct {
+	conn net.Conn
+}
 
 // ReadUntilCRLF reads from the connection until it finds a CRLF sequence.
 // It returns the string up to the CRLF sequence.
-func ReadUntilCRLF(conn net.Conn) (string, error) {
+func (c *ConnHandler) ReadUntilCRLF() (string, error) {
 	var data []byte
 
 	for {
 		aByte := make([]byte, 1)
-		_, err := conn.Read(aByte)
+		_, err := c.conn.Read(aByte)
 		if err == io.EOF {
 			// Connection closed by peer
 			return string(data), io.EOF
@@ -39,16 +53,9 @@ func ReadUntilCRLF(conn net.Conn) (string, error) {
 	}
 }
 
-// RequestLine represents the details of the request like http method, target, and http version.
-type RequestLine struct {
-	HTTPMethod    string
-	RequestTarget string
-	HTTPVersion   string
-}
-
 // ReadRequestLine reads the request line from the request connection
-func ReadRequestLine(conn net.Conn) (*RequestLine, error) {
-	rawReqLine, err := ReadUntilCRLF(conn)
+func (c *ConnHandler) ReadRequestLine() (*RequestLine, error) {
+	rawReqLine, err := c.ReadUntilCRLF()
 	if err != nil {
 		fmt.Println("Error reading the request line: ", err)
 		return nil, err
@@ -70,11 +77,11 @@ func ReadRequestLine(conn net.Conn) (*RequestLine, error) {
 type RequestHeader map[string]string
 
 // ReadRequestHeader reads the request header from the request connection
-func ReadRequestHeader(conn net.Conn) (RequestHeader, error) {
+func (c *ConnHandler) ReadRequestHeader() (RequestHeader, error) {
 	reqHeader := make(map[string]string)
 
 	for {
-		header, err := ReadUntilCRLF(conn)
+		header, err := c.ReadUntilCRLF()
 		if err != nil {
 			fmt.Println("Error reading the header: ", err)
 			return nil, err
@@ -96,70 +103,113 @@ func ReadRequestHeader(conn net.Conn) (RequestHeader, error) {
 }
 
 // SendResponse sends the given resp to the client
-func SendResponse(conn net.Conn, resp []byte) {
-	if _, err := conn.Write(resp); err != nil {
+func (c *ConnHandler) SendResponse(resp []byte) {
+	if _, err := c.conn.Write(resp); err != nil {
 		fmt.Println("Error returning response: ", err)
 		os.Exit(1)
 	}
 }
 
 // RootHandler handles the root endpoint
-func RootHandler(conn net.Conn) {
-	SendResponse(conn, []byte("HTTP/1.1 200 OK\r\n\r\n"))
+func (c *ConnHandler) RootHandler() {
+	c.SendResponse([]byte("HTTP/1.1 200 OK\r\n\r\n"))
 }
 
 // NotFoundHandler handles the endpoint not found
-func NotFoundHandler(conn net.Conn) {
-	SendResponse(conn, []byte("HTTP/1.1 404 Not Found\r\n\r\n"))
+func (c *ConnHandler) NotFoundHandler() {
+	c.SendResponse([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
 }
 
 // EchoHandler handles the request for /echo/<str> endpoint
-func EchoHandler(conn net.Conn, reqLine *RequestLine) {
+func (c *ConnHandler) EchoHandler(reqLine *RequestLine) {
 	matches := EchoEndpointRegx.FindStringSubmatch(reqLine.RequestTarget)
-	SendResponse(conn, fmt.Appendf(nil, "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(matches[1]), matches[1]))
+	c.SendResponse(fmt.Appendf(nil, "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(matches[1]), matches[1]))
 }
 
-// UserAgentHandler handles the request for /user-endpoint header
-func UserAgentHandler(conn net.Conn, reqHeader RequestHeader) {
+// UserAgentHandler handles the request for /user-endpoint endpoint
+func (c *ConnHandler) UserAgentHandler(reqHeader RequestHeader) {
 	val, ok := reqHeader["User-Agent"]
 	if !ok {
 		fmt.Println("No 'User-Agent' header present!")
 		os.Exit(1)
 	}
 
-	SendResponse(conn, fmt.Appendf(nil, "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(val), val))
+	c.SendResponse(fmt.Appendf(nil, "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(val), val))
+}
+
+// FileHandler handles the request for /files/{filename} endpoint
+func (c *ConnHandler) FileHandler(flags map[string]any, reqLine *RequestLine) {
+	dir, ok := flags["directory"].(string)
+	if !ok {
+		fmt.Println("No director provided to serve the file")
+		os.Exit(1)
+	}
+	// Check if the directory exists or not
+	if info, err := os.Stat(dir); err != nil || !info.IsDir() {
+		fmt.Println("Directory doesn't exists")
+		os.Exit(1)
+	}
+
+	filename := FileEndpointRegx.FindStringSubmatch(reqLine.RequestTarget)[1]
+	// Open the file
+	file, err := os.Open(dir + "/" + filename)
+	if err != nil {
+		fmt.Println("Error opening the file: ", err.Error())
+		os.Exit(1)
+	}
+	defer file.Close()
+
+	// Read the file
+	content, err := io.ReadAll(file)
+	if err != nil {
+		fmt.Println("Failed to read the file: ", err.Error())
+		os.Exit(1)
+	}
+
+	c.SendResponse(fmt.Appendf(nil, "HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: %d\r\n\r\n%s", len(content), string(content)))
 }
 
 // HandleConnection handles the single connect request
-func HandleConnection(conn net.Conn) {
-	defer conn.Close()
+func (c *ConnHandler) HandleConnection(flags map[string]any) {
+	defer c.conn.Close()
 
 	// Read the request line from the connection
-	reqLine, err := ReadRequestLine(conn)
+	reqLine, err := c.ReadRequestLine()
 	if err != nil {
 		fmt.Println("Error reading the request line: ", err)
 		os.Exit(1)
 	}
 
 	// Read the request header
-	reqHeader, err := ReadRequestHeader(conn)
+	reqHeader, err := c.ReadRequestHeader()
 	if err != nil {
 		fmt.Println("Error reading the connection: ", err)
 		os.Exit(1)
 	}
 
 	if reqLine.RequestTarget == "/" {
-		RootHandler(conn)
+		c.RootHandler()
 	} else if reqLine.RequestTarget == "/user-agent" {
-		UserAgentHandler(conn, reqHeader)
+		c.UserAgentHandler(reqHeader)
 	} else if matches := EchoEndpointRegx.FindStringSubmatch(reqLine.RequestTarget); len(matches) > 0 {
-		EchoHandler(conn, reqLine)
+		c.EchoHandler(reqLine)
+	} else if matches := FileEndpointRegx.FindStringSubmatch(reqLine.RequestTarget); len(matches) > 0 {
+		c.FileHandler(flags, reqLine)
 	} else {
-		NotFoundHandler(conn)
+		c.NotFoundHandler()
 	}
 }
 
 func main() {
+	// Directory flag for the /files/{filename} endpoint
+	directory := flag.String("directory", "", "The directory from which files should be served.")
+	// Parse the CLI args to populate the flag variables.
+	flag.Parse()
+	// Store it in the map
+	flags := map[string]any{
+		"directory": *directory,
+	}
+
 	// Creates an HTTP server
 	l, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
@@ -176,7 +226,10 @@ func main() {
 			os.Exit(1)
 		}
 
+		c := &ConnHandler{
+			conn,
+		}
 		// Handle the connection in a separate goroutine
-		go HandleConnection(conn)
+		go c.HandleConnection(flags)
 	}
 }
